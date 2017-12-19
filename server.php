e70b63f701d55658e7f715767aa825a23ddd788b
Paul <dePhantom@online-training-school.com>
2017-12-19 14:35:35 -0600

    clean up

--- server.php
+++ server.php
@@ -10,7 +10,6 @@
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
-echo "Hello world";
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
